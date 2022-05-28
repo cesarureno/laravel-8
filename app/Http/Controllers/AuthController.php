5d4441f514ec244aeb368c37bc89571e89495f99
César Ureño <cesarurenomartinez@gmail.com>
2022-05-28 13:32:58 -0500

    feat: :sparkles: Return all data related with corporation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Passport\Passport;
 
 class AuthController extends Controller
 {
@@ -18,12 +19,15 @@ class AuthController extends Controller
 
             switch ($user->role_id) {
                 case 1:
+                    Passport::personalAccessTokensExpireIn(now()->addWeek());
                     array_push($scopes, 'corporations', 'companies');
                     break;
                 case 2:
+                    Passport::personalAccessTokensExpireIn(now()->addWeeks(2));
                     array_push($scopes, 'contacts', 'contracts');
                     break;
                 case 3:
+                    Passport::personalAccessTokensExpireIn(now()->addYear());
                     array_push($scopes, 'documents', 'corporation-document');
                     break;
             }
@@ -31,9 +35,9 @@ class AuthController extends Controller
             $data['user'] = $user;
             $data['access_token'] = $user->createToken('Laravel 8', $scopes)->accessToken;
 
-            return response()->json($data, 200);
+            return response()->success($data, 200);
         } else {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->error(['error' => 'Invalid credentials'], 401, "Unauthorized");
         }
     }
 }
